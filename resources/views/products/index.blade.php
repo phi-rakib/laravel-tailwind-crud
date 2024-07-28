420bb04fd7ff11fcdcb39822aeea1c2cfe3c8c16
rakib <phi.rakib@gmail.com>
2024-07-28 22:36:23 +0600

    added product index page show, edit & delete button hover style

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -24,12 +24,12 @@
                 <td class="border px-4 py-2">{{ $product->name }}</td>
                 <td class="border px-4 py-2">{{ $product->price }}</td>
                 <td class="border px-4 py-2">
-                    <a href="{{ route('products.show', $product->id) }}" class="bg-green-500 text-white px-2 py-1 rounded-md">View</a>
-                    <a href="{{ route('products.edit', $product->id) }}" class="bg-yellow-500 text-white px-2 py-1 rounded-md">Edit</a>
+                    <a href="{{ route('products.show', $product->id) }}" class="bg-green-500 hover:bg-green-600 text-white px-2 py-1 rounded-md">View</a>
+                    <a href="{{ route('products.edit', $product->id) }}" class="bg-yellow-500 hover:bg-yellow-600 text-white px-2 py-1 rounded-md">Edit</a>
                     <form action="{{ route('products.destroy', $product->id) }}" method="POST" class="inline">
                         @csrf
                         @method('DELETE')
-                        <button type="submit" class="bg-red-500 text-white px-2 py-1 rounded-md">Delete</button>
+                        <button type="submit" class="bg-red-500 hover:bg-red-600 text-white px-2 py-1 rounded-md">Delete</button>
                     </form>
                 </td>
             </tr>
